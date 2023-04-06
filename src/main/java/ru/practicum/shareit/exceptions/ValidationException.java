f4470e5f6a002fcdac93a643c0acd742f64525f7
Artem <ustavshikovartem@yandex.ru>
2023-04-07 02:15:40 +0300

    add-controllers_Версия 1

--- src/main/java/ru/practicum/shareit/exceptions/ValidationException.java
+++ src/main/java/ru/practicum/shareit/exceptions/ValidationException.java
@@ -1,6 +1,6 @@
 package ru.practicum.shareit.exceptions;
 
-public class ValidationException extends RuntimeException{
+public class ValidationException extends RuntimeException {
     public ValidationException(String message) {
         super(message);
     }
