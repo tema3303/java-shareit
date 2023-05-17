65ccf468eb3cadc552c8bc49cc8ddcb3a950c1ed
Artem <ustavshikovartem@yandex.ru>
2023-05-17 05:37:33 +0300

    add_tests

--- src/test/java/ru/practicum/shareit/users/service/UserServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/users/service/UserServiceImplTest.java
@@ -6,7 +6,6 @@ import org.mockito.AdditionalAnswers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import ru.practicum.shareit.exceptions.ValidationException;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.model.dto.UserDto;
 import ru.practicum.shareit.user.model.dto.UserMapper;
