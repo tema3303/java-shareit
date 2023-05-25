ea6afe400adc23c5ba3e75dfc9d1342bbfcd9881
Artem <ustavshikovartem@yandex.ru>
2023-05-25 20:36:40 +0300

    add-docker_Откат к предыдущей версии

--- gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.booking;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
@@ -23,31 +22,31 @@ import javax.validation.constraints.PositiveOrZero;
 @Validated
 public class BookingController {
     private final BookingClient bookingClient;
-
+    private static final String USER_ID = "X-Sharer-User-Id";
 
     @PostMapping
-    public ResponseEntity<Object> saveBooking(@RequestHeader @Value("${USER_ID}") long userId,
+    public ResponseEntity<Object> saveBooking(@RequestHeader(value = USER_ID) long userId,
                                               @RequestBody @Valid BookItemRequestDto requestDto) {
         log.info("Creating booking {}, userId={}", requestDto, userId);
         return bookingClient.saveBooking(userId, requestDto);
     }
 
     @PatchMapping("/{bookingId}")
-    public ResponseEntity<Object> confirmBooking(@RequestHeader @Value("${USER_ID}") Long userId,
+    public ResponseEntity<Object> confirmBooking(@RequestHeader(value = USER_ID) Long userId,
                                                  @PathVariable Long bookingId, @RequestParam Boolean approved) {
         log.info("Update booking {}, userId={}", bookingId, userId);
         return bookingClient.confirmBooking(userId, bookingId, approved);
     }
 
     @GetMapping("/{bookingId}")
-    public ResponseEntity<Object> getBookingById(@RequestHeader @Value("${USER_ID}") long userId,
+    public ResponseEntity<Object> getBookingById(@RequestHeader(value = USER_ID) long userId,
                                                  @PathVariable Long bookingId) {
         log.info("Get booking {}, userId={}", bookingId, userId);
         return bookingClient.getBookingById(userId, bookingId);
     }
 
     @GetMapping
-    public ResponseEntity<Object> getAllBooking(@RequestHeader @Value("${USER_ID}") long userId,
+    public ResponseEntity<Object> getAllBooking(@RequestHeader(value = USER_ID) long userId,
                                                 @RequestParam(name = "state", defaultValue = "all") String stateParam,
                                                 @PositiveOrZero @RequestParam(name = "from", defaultValue = "0") Integer from,
                                                 @Positive @RequestParam(name = "size", defaultValue = "10") Integer size) {
@@ -58,7 +57,7 @@ public class BookingController {
     }
 
     @GetMapping("/owner")
-    public ResponseEntity<Object> getAllBookingForItems(@RequestHeader @Value("${USER_ID}") long userId,
+    public ResponseEntity<Object> getAllBookingForItems(@RequestHeader(value = USER_ID) long userId,
                                                         @RequestParam(name = "state", defaultValue = "all") String stateParam,
                                                         @PositiveOrZero @RequestParam(name = "from", defaultValue = "0") Integer from,
                                                         @Positive @RequestParam(name = "size", defaultValue = "10") Integer size) {
